3b2c61e92570acec4a00128e58ab7d986b7b9a32
yayarusli <152648584+yayarusli@users.noreply.github.com>
2025-05-22 15:43:07 +0800

    Update script2.js

--- script2.js
+++ script2.js
@@ -14,18 +14,18 @@ function searchNews() {
       return response.json();
     })
     .then(data => {
-      if (!data.articles || data.articles.length === 0) {
+      if (!data.data || data.data.length === 0) {
         container.innerHTML = "<p class='text-center'>No news found.</p>";
         return;
       }
 
-      data.articles.forEach(article => {
+      data.data.forEach(article => {
         const card = document.createElement("div");
         card.className = "col-md-6";
 
         card.innerHTML = `
           <div class="card">
-            ${article.urlToImage ? `<img src="${article.urlToImage}" class="card-img-top news-img" alt="News Image" />` : ""}
+            ${article.image ? `<img src="${article.image}" class="card-img-top news-img" alt="News Image" />` : ""}
             <div class="card-body">
               <h5 class="card-title">${article.title}</h5>
               <p class="card-text">${article.description || "No description available."}</p>
